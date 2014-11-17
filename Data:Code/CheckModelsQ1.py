690954601b1ece3534e09b79f66fefdee7a912cc
JoshTycko <joshtycko@gmail.com>
2014-11-17 18:03:17 -0500

    running linear regression

--- Data:Code/CheckModelsQ1.py
+++ Data:Code/CheckModelsQ1.py
@@ -9,16 +9,23 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn import linear_model
+import pylab as pl
 
 
 # load the data set
-filename = 'AD_Challenge_Training_Data_Clinical_Updated_7/ADNI_Training_Q1_APOE_July22.2014_SPENCER_Edit.csv'
+filename = 'AD_Challenge_Training_Data_Clinical_Updated_7/ADNI_Training_Q1_APOE_July22.2014_JOSH_Edit.csv'
 
+#way to load while keeping headers as dtypes for each column
+#X = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, usecols = range(0,12)) #all the data except MMSE_24
+#y = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, usecols=(12)) #output is the MMSE after 24 months
 
-X = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, usecols = range(0,12)) #all the data except MMSE_24
-y = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, usecols=(12)) #output is the MMSE after 24 months
-
-n = len(X)
+#load everything as floats without headers
+data = np.loadtxt(filename, delimiter =',', skiprows=1)
+n,d = data.shape
+X = data[:,0:d-1]
+y=data[:,d-1]
 nTrain = 0.5*n  #training on 50% of the data
 
 # shuffle the data
@@ -34,34 +41,65 @@ ytrain = y[:nTrain]
 Xtest = X[nTrain:]
 ytest = y[nTrain:]
 
-#build Model SVM
-modelSVM = svm.SVC(C = 3, kernel = linear_kernel)
-start2 = time.time()
-modelSVM.fit(Xtrain, ytrain)
-end2 = time.time()
+
+
+    # #build Model SVM
+    # modelSVM = svm.SVC(C = 3, kernel = linear_kernel)
+    # start2 = time.time()
+    # modelSVM.fit(Xtrain, ytrain)
+    # end2 = time.time()
+    #
+    # #Make Predictions
+    # TrainPredSVM = modelSVM.predict(Xtrain)
+    # TestPredSVM = modelSVM.predict(Xtest)
+    #
+    # #metrics
+    # TrainAccSVM = accuracy_score(ytrain, TrainPredSVM)
+    # TestAccSVM = accuracy_score(ytest, TestPredSVM)
+    #
+    # TrainPrecSVM = precision_score(ytrain, TrainPredSVM)
+    # TestPrecSVM = precision_score(ytest, TestPredSVM)
+    #
+    # TrainRecallSVM = recall_score(ytrain, TrainPredSVM)
+    # TestRecallSVM = recall_score(ytest, TestPredSVM)
+    #
+    # TrainTimeSVM = end2 - start2
+    #
+    # print '...SVM Metrics...'
+    # print 'training acc: ', TrainAccSVM
+    # print 'testing acc: ', TestAccSVM
+    # print 'train precision: ',TrainPrecSVM
+    # print 'test precision : ',TestPrecSVM
+    # print 'train recall: ', TrainRecallSVM
+    # print 'test recall: ', TestRecallSVM
+    # print 'train time: ', TrainTimeSVM, ' seconds'
+
+#build Linear Regression Model
+modelLin = linear_model.LinearRegression()
+modelLin.fit(Xtrain, ytrain)
 
 #Make Predictions
-TrainPredSVM = modelSVM.predict(Xtrain)
-TestPredSVM = modelSVM.predict(Xtest)
+TrainPredLin = modelLin.predict(Xtrain)
+TestPredLin = modelLin.predict(Xtest)
 
-#metrics
-TrainAccSVM = accuracy_score(ytrain, TrainPredSVM)
-TestAccSVM = accuracy_score(ytest, TestPredSVM)
+#print TrainPredLin[0:10], ytrain[0:10]
 
-TrainPrecSVM = precision_score(ytrain, TrainPredSVM)
-TestPrecSVM = precision_score(ytest, TestPredSVM)
 
-TrainRecallSVM = recall_score(ytrain, TrainPredSVM)
-TestRecallSVM = recall_score(ytest, TestPredSVM)
+# The coefficients
+print 'Coefficients: \n', modelLin.coef_
+# The mean square error
+print ("Residual sum of squares: %.2f" %
+        np.mean((modelLin.predict(Xtest) - ytest) ** 2))
+# Explained variance score: 1 is perfect prediction
+print ('Variance score: %.2f' % modelLin.score(Xtest, ytest))
 
-TrainTimeSVM = end2 - start2
-
-print '...SVM Metrics...'
-print 'training acc: ', TrainAccSVM
-print 'testing acc: ', TestAccSVM
-print 'train precision: ',TrainPrecSVM
-print 'test precision : ',TestPrecSVM
-print 'train recall: ', TrainRecallSVM
-print 'test recall: ', TestRecallSVM
-print 'train time: ', TrainTimeSVM, ' seconds'
+# Plot outputs
+# pl.scatter(Xtest, ytest,  color='black')
+# pl.plot(Xtest, modelLin.predict(Xtest), color='blue',
+#         linewidth=3)
+#
+# pl.xticks(())
+# pl.yticks(())
+#
+# pl.show()
 
